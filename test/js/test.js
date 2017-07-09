16fb37d5b4c1ff51a5531baa2ce5eeb843ae77d2
Travis Tran <espringtran@gmail.com>
2017-07-09 10:08:53 +0700

    test page include Roboto

--- test/js/test.js
+++ test/js/test.js
@@ -3,21 +3,55 @@
  */
 (function () {
     'use strict';
-    angular.module('App', ['ngResource', 'ngCookies', 'ngMaterial'])
-        .config(function($mdThemingProvider, $mdAriaProvider) {
-            $mdThemingProvider.theme('default');
-        })
-        .controller('AppCtrl', function($scope) {
+    angular.module('App', ['ngMaterial'])
+        .controller('AppCtrl', function ($scope) {
             // Define functions
+            $scope.loadByFamily = function (family) {
+                for (var i = $scope.fonts.length - 1; i >= 0; i--) {
+                    var font = $scope.fonts[i];
+                    if (font.family == family) {
+                        $scope.fontWeights = angular.copy(font.weights);
+                        $scope.fontStyles = angular.copy(font.styles);
+                        $scope.formValue.fontWeight = $scope.fontWeights[0];
+                        $scope.formValue.fontStyle = $scope.fontStyles[0];
+                        break;
+                    }
+                }
+            };
             // Initialization Routine
-            $scope.fontFamilies = ['Noto Sans', 'Noto Sans UI', 'Noto Serif'];
-            $scope.fontWeights = ['400', '700', 'normal', 'bold', 'bolder', 'lighter'];
-            $scope.fontStyles = ['normal', 'italic'];
-            $scope.formValue = {}
+            $scope.fonts = [{
+                family: 'Noto Sans',
+                weights: [400, 700],
+                styles: ['normal', 'italic']
+            }, {
+                family: 'Noto Sans UI',
+                weights: [400, 700],
+                styles: ['normal', 'italic']
+            }, {
+                family: 'Noto Serif',
+                weights: [400, 700],
+                styles: ['normal', 'italic']
+            }, {
+                family: 'Roboto',
+                weights: [100, 300, 400, 500, 700, 900],
+                styles: ['normal', 'italic']
+            }, {
+                family: 'Roboto Condensed',
+                weights: [300, 400, 700],
+                styles: ['normal', 'italic']
+            }];
+            $scope.fontFamilies = [];
+            $scope.fontWeights = [];
+            $scope.fontStyles = [];
+            for (var i = 0; i < $scope.fonts.length; i++) {
+                $scope.fontFamilies.push($scope.fonts[i].family);
+            }
+            $scope.formValue = {};
             $scope.formValue.fontFamily = $scope.fontFamilies[0];
             $scope.formValue.fontWeight = $scope.fontWeights[0];
             $scope.formValue.fontStyle = $scope.fontStyles[0];
             $scope.formValue.fontSize = 24;
             $scope.formValue.text = 'Here is preview text.';
+            $scope.loadByFamily($scope.formValue.fontFamily);
         });
 })();
